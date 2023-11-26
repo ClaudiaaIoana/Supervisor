e6dae1d9d3f117794ae85d0c890a8a4e4ba7a1c0
ClaudiaaIoana <claudia.dascalescu2909@gmail.com>
2023-11-26 17:39:21 +0200

    interpret conf

--- client/test1.c
+++ client/test1.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
 
 int main() {
     int uid, euid;
     int gid, egid;
+    char    buffer[50];
+
+    scanf("%s",buffer);
+    printf("BUFFER: %s ??",buffer);
 
     // Get real user and group IDs
     uid = getuid();
@@ -18,6 +24,30 @@ int main() {
     printf("Real Group ID: %d\n", gid);
     printf("Effective User ID: %d\n", euid);
     printf("Effective Group ID: %d\n", egid);
+
+    int pid = getpid();  // Get the process ID of the current process
+
+    int priority = getpriority(PRIO_PROCESS, pid);
+
+    if (priority == -1) {
+        perror("Error getting process priority");
+        return 1;
+    }
+
+    printf("Process priority: %d\n", priority);
+
+    char cwd[1024];
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("Error getting current working directory");
+        return 1;
+    }
+
+    printf("Current working directory: %s\n", cwd);
+
+    int mask=umask(0);
+
+    printf("Current mask : %o\n",mask);
 
     return 0;
 }
