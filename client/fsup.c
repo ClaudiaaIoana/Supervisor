49eb3bb5c1b6e4774b8f727c2d92559e4f173e52
ClaudiaaIoana <claudia.dascalescu2909@gmail.com>
2023-11-28 19:40:54 +0200

    start connection

---
+++ client/fsup.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <signal.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <grp.h>
+#include <pwd.h>
+
+
+int main()
+{
+    
+
+    return 0;
+}
+
