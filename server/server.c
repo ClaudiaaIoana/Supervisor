9b9fe1295ea38b98f1d2dbeb68d9bcd22a492b41
sebi-d <andrei.dodot@mta.ro>
2023-12-12 13:49:07 +0200

    added multiple-clients support by io multiplexing

--- server/server.c
+++ server/server.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -5,6 +6,8 @@
 #include <unistd.h>
 #include <time.h>
 #include <pthread.h>
+#include <poll.h>
+#include <fcntl.h>
 
 #include "../utils/utils.h"
 #include "../utils/json.h"
@@ -16,34 +19,45 @@
 typedef struct Process{
     pid_t pid;
     char *name;
+    int client_fd;
 } Process;
 
+typedef struct Connection {
+    __uint16_t port;
+    struct sockaddr_in serv_addr;
+    socklen_t socket_len;
+    size_t num_clients;
+    struct pollfd *listen;
+    struct pollfd *clients;
+} Connection;
+
 typedef struct State{
+    Connection* connection;
     Process *processes;
     size_t process_count;
     size_t capacity;
     FILE *log;
 } State;
 
-typedef struct Connection {
-    __uint16_t port;
-    struct sockaddr_in serv_addr;
-    socklen_t socket_len;
-    int listen_fd;
-    
-} Connection;
+typedef struct Task {
+    int client_fd;
+    void* (*function)(void*);
+    void *args;
+} Task;
 
 pthread_mutex_t mutex_clients = PTHREAD_MUTEX_INITIALIZER;
-
-int clients_fd[MAX_CLIENTS] = { 0 };
+pthread_cond_t cond_clients = PTHREAD_COND_INITIALIZER;
 
 time_t current_time;
 
-int main(int argc, char * argv[]);
-void init_manager_state(State *manager);
+int main(int argc, char *argv[]);
+void init_connection(Connection *connection);
+void init_manager_state(State *manager, Connection *connection);
 void add_process(State *manager, pid_t pid, const char *process_name);
 void print_processes(const State *manager);
+void print_clients(const State *manager);
 void cleanup_state(State *manager);
+void cleanup_connection(Connection *connection);
 void log_entry(State *manager, const char *message);
 void *client_handler(void *args);
 void *connection_handler(void* args);
@@ -56,17 +70,18 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    Connection *connection = (Connection*) calloc(1, sizeof(Connection));
+    Connection *connection = (Connection*) calloc (1, sizeof(Connection));
     DIE(connection == NULL, "calloc()");
     connection->port = atoi(argv[2]);
-
-    connection->listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-    DIE(connection->listen_fd < 0, "socket()");
+    init_connection(connection);
+    
+    connection->listen->fd = socket(AF_INET, SOCK_STREAM, 0);
+    DIE(connection->listen->fd < 0, "socket()");
 
     connection->socket_len = sizeof(struct sockaddr_in);
 
     int enable = 1;
-    if(setsockopt(connection->listen_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
+    if(setsockopt(connection->listen->fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
         perror("setsockopt(SO_REUSEADDR) failed");
     }
 
@@ -77,7 +92,7 @@ int main(int argc, char *argv[]) {
     int rc = inet_pton(AF_INET, argv[1], &connection->serv_addr.sin_addr.s_addr);
     DIE(rc <= 0, "inet_pton");
 
-    rc = bind(connection->listen_fd, (const struct sockaddr*)&connection->serv_addr, connection->socket_len);
+    rc = bind(connection->listen->fd, (const struct sockaddr*)&connection->serv_addr, connection->socket_len);
     DIE(rc < 0, "bind()");
 
     pthread_t connection_thread;
@@ -87,7 +102,7 @@ int main(int argc, char *argv[]) {
     State *manager = (State*) calloc (1, sizeof(State));
     DIE(manager == NULL, "calloc()");
 
-    init_manager_state(manager);
+    init_manager_state(manager, connection);
 
     pthread_t thread_pool[MAX_CLIENTS];
     for (size_t i = 0; i < MAX_CLIENTS; i++) {
@@ -104,7 +119,6 @@ int main(int argc, char *argv[]) {
         pthread_join(thread_pool[i], NULL);
     }
     pthread_join(server_cmd, NULL);
-
     
     cleanup_state(manager);
     free(manager);
@@ -114,26 +128,41 @@ int main(int argc, char *argv[]) {
 
 void *connection_handler(void *args)
 {   
-    int num_clients = 0;
     Connection *connection = (Connection*) args;
-    int rc = listen(connection->listen_fd, MAX_CLIENTS);
+    int rc = listen(connection->listen->fd, MAX_CLIENTS);
     DIE(rc < 0, "listen()");
-
-    printf("Waiting for client...\n");
     while(1) {
-        if(num_clients == MAX_CLIENTS) {
-            break;
+        rc = poll(connection->listen, 1, 0);
+        DIE(rc < 0, "poll_listen()");
+        if(connection->listen->revents & POLLIN) {
+            printf("added client: %i\n", connection->listen->revents);
+            struct sockaddr_in client_addr;
+            socklen_t client_len = sizeof(client_addr);
+            int conn_fd = accept(connection->listen->fd, (struct sockaddr*)&client_addr, &client_len);
+            DIE(conn_fd < 0, "accept()");
+            printf("new conn_fd: %d\n", conn_fd);
+            printf("num_clients: %d\n", connection->num_clients);
+            //client->ip_address = inet_ntoa(client_addr.sin_addr));
+            pthread_mutex_lock(&mutex_clients);
+            connection->clients[connection->num_clients].fd = conn_fd;
+            connection->clients[connection->num_clients].events = POLLIN | POLLRDHUP;
+            connection->num_clients++;
+            connection->listen->revents = 0;
+            pthread_mutex_unlock(&mutex_clients); 
         }
-        struct sockaddr_in client_addr;
-        socklen_t client_len = sizeof(client_addr);
-        int conn_fd = accept(connection->listen_fd, (struct sockaddr*)&client_addr, &client_len);
-        DIE(conn_fd < 0, "accept()");
-
-        pthread_mutex_lock(&mutex_clients);
-        clients_fd[num_clients] = conn_fd;
-        num_clients++;
-        pthread_mutex_unlock(&mutex_clients);
-        printf("New client connected...\n");
+        rc = poll(connection->clients, MAX_CLIENTS, 0);
+        DIE(rc < 0, "poll_clients()");
+        for (size_t i = 0; i < MAX_CLIENTS; i++) {
+            if(connection->clients[i].fd != 0) {
+                if(connection->clients[i].revents & POLLRDHUP) {
+                    printf("client disconnected\n");
+                    close(connection->clients[i].fd);
+                    connection->clients[i].fd = 0;
+                    connection->num_clients--;
+                    connection->clients[i].revents = 0;
+                }
+            }
+        }
     }
 }
 
@@ -143,20 +172,24 @@ void *client_handler(void *args)
     char buffer[BUFFER_SIZE];
     while(1) {
         for (size_t i = 0; i < MAX_CLIENTS; i++) {
-            if(clients_fd[i] != 0) {
-                int rc = recv(clients_fd[i], buffer, BUFFER_SIZE, 0);
-                if(rc <= 0) {
-                    break;
+            if(manager->connection->clients[i].fd != 0) {
+                if(manager->connection->clients[i].revents & POLLIN) {
+                    printf("client %d sent data\n", manager->connection->clients[i].fd);
+                    manager->connection->clients[i].revents = 0;
+                    int rc = recv(manager->connection->clients[i].fd, buffer, BUFFER_SIZE, 0);
+                    if(rc <= 0) {
+                        break;
+                    }
+                    buffer[rc] = '\0';
                 }
-                buffer[rc] = '\0';
                 // access information in JSON data
-                cJSON *json = cJSON_Parse(buffer);   
-                cJSON *pid = cJSON_GetObjectItem(json, "pid");
-                cJSON *process_name = cJSON_GetObjectItem(json, "name");
-                char message[32];
-                strcpy(message, "process launched");
-                log_entry(manager, message);
-                add_process(manager, pid->valueint, process_name->valuestring); 
+                //cJSON *json = cJSON_Parse(buffer);   
+                //cJSON *pid = cJSON_GetObjectItem(json, "pid");
+                //cJSON *process_name = cJSON_GetObjectItem(json, "name");
+                //char message[32];
+                //strcpy(message, "process launched");
+                //log_entry(manager, message);
+                //add_process(manager, pid->valueint, process_name->valuestring); 
             }
         }
     }
@@ -166,30 +199,49 @@ void *server_command(void *args)
 {
     State *manager = (State*) args;
     char buffer[BUFFER_SIZE];
+
     while(1) {
-        printf("> ");
-        fflush(stdout);
+        
+        printf("supervisor> ");
+        memset(buffer, 0 , BUFFER_SIZE);
         fgets(buffer, BUFFER_SIZE, stdin);
         buffer[strlen(buffer) - 1] = '\0';
+        
         if(strcmp(buffer, "exit") == 0) {
             break;
         }
         else if(strcmp(buffer, "status") == 0) {
             print_processes(manager);
         }
-        else {
+        else if(strcmp(buffer, "clients") == 0) {
+            print_clients(manager);
+        } 
+        else{
             printf("invalid command\n");
         }
     }
 }
 
-void init_manager_state(State *manager)
+void init_connection(Connection *connection)
 {
+    connection->listen = calloc(1, sizeof(struct pollfd));
+    DIE(connection->listen == NULL, "calloc()");
+    connection->listen->events = POLLIN;
+    connection->clients = calloc(MAX_CLIENTS, sizeof(struct pollfd));
+    DIE(connection->clients == NULL, "calloc()");
+    connection->num_clients = 0;
+}
+
+void init_manager_state(State *manager, Connection *connection)
+{
+    manager->connection = connection;
     manager->processes = calloc(manager->capacity, sizeof(Process));
     manager->process_count = 0;
     manager->capacity = 256;
-    manager->log = fopen("manager.log", "a");
+    manager->log = fopen("manager.log", "w");
     DIE(manager->log == NULL, "fopen()");
+    manager->connection->clients = calloc(MAX_CLIENTS, sizeof(struct pollfd));
+    DIE(manager->connection->clients == NULL, "calloc()");
 }
 
 void add_process(State *manager, pid_t pid, const char *process_name) {
@@ -206,15 +258,30 @@ void log_entry(State *manager, const char *message) {
 }
 
 void print_processes(const State *manager) {
-    printf("current processes for connected client:\n");
+    printf("current processes for client:\n");
     for (size_t i = 0; i < manager->process_count; i++) {
         const Process *process = &manager->processes[i];
         printf("pid: <%d>, name: <%s>\n", process->pid, process->name);
     }
 }
 
+void print_clients(const State *manager) {
+    for (size_t i = 0; i < manager->connection->num_clients; i++) {
+        if(manager->connection->clients[i].fd != 0) {
+            printf("client: <%d>\n", manager->connection->clients[i].fd);
+        }
+    }
+}
+
 void cleanup_state(State *manager) {
     free(manager->processes);
+    free(manager->connection->clients);
     manager->process_count = 0;
     manager->capacity = 256;
+}
+
+void cleanup_connection(Connection *connection) {
+    free(connection->listen);
+    free(connection->clients);
+    connection->num_clients = 0;
 }
